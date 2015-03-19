3ced044aa8c2f1062be954b155c93488bb910097
Jon Rose <jonrose08@gmail.com>
2015-03-18 19:37:58 -0600

    add reagent

--- src/cljs/bracketology/core.cljs
+++ src/cljs/bracketology/core.cljs
@@ -1,7 +1,22 @@
 (ns bracketology.core
   (:require [reagent.core :as r :refer [atom]]))
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(defonce app-state* (atom {:text "Hello Chestnut!"}))
+
+
+(comment
+
+  (reset! app-state* {:text "FooBar"})
+
+
+  )
+
+(defn app-view
+  [app]
+  [:div.container
+   [:p (:text @app)]])
+
 
 (defn main []
-  (r))
+  (r/render-component [app-view app-state*]
+                      (.getElementById js/document "app")))
