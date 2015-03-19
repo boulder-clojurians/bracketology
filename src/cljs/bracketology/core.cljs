3b82cc38c91306f04c830766e8c334fbd4295a44
Jon Rose <jonrose08@gmail.com>
2015-03-18 20:37:32 -0600

    update

--- src/cljs/bracketology/core.cljs
+++ src/cljs/bracketology/core.cljs
@@ -5,38 +5,66 @@
 (enable-console-print!)
 
 (defonce app-state* (atom {:text "Hello FooBar!"
-                           :brackets [["foo"
-                                       "bar"
-                                       "biz"
-                                       "boz"]
-                                      ["aaa"
-                                       "bbb"
-                                       "ccc"
-                                       "ddd"]
-                                      ["xxx"
-                                       "yyy"
-                                       "zzz"
-                                       "qqq"]
-                                      ["xxx"
-                                       "yyy"
-                                       "zzz"
-                                       "qqq"]]}))
+                           :brackets [
+                                      [[["goo"
+                                         "bar"]
+                                        ["biz"
+                                         "boz"]]
+                                       [["foo"
+                                         "biz"]]
+                                       [["biz"]]]
+                                      [[["soo"
+                                         "bar"]
+                                        ["biz"
+                                         "boz"]]
+                                       [["foo"
+                                         "biz"]]
+                                       [["biz"]]]
+                                      [[["hoo"
+                                         "bar"]
+                                        ["biz"
+                                         "boz"]]
+                                       [["foo"
+                                         "biz"]]
+                                       [["biz"]]]
+                                      [[["roo"
+                                         "bar"]
+                                        ["biz"
+                                         "boz"]]
+                                       [["foo"
+                                         "biz"]]
+                                       [["biz"]]]
+                                      ]}))
 
+
+
+(defn game
+  [g]
+  [:ul.game
+   [:li.team (first g)]
+   (when (next g)
+     [:li.team (last g)])])
 
 
 (defn bracket
-  [teams]
+  [games idx]
+  (println idx)
   [:ul.col-sm-6
-   (for [team teams]
-     [:li.team team])])
+   (for [round (if (even? idx) games (reverse games))]
+     ^{:key round}
+     [:div.round
+      (for [gm round]
+        ^{:key gm}
+        [game gm])])])
 
 
 (defn app-view
   [app]
   [:div.container
    [:h1 "Bracket"]
-   (for [b (:brackets @app)]
-     [bracket b])])
+   (for [b (:brackets @app) idx (range (count (:brackets @app)))]
+     ^{:key b}
+     [bracket b idx])])
 
 
 (defn main []
