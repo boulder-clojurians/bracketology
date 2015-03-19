1744bdb4f721e8ce1ffb167f93085bc4e0d4539d
Jon Rose <jonrose08@gmail.com>
2015-03-18 20:07:54 -0600

    add dummy teams

--- src/cljs/bracketology/core.cljs
+++ src/cljs/bracketology/core.cljs
@@ -1,20 +1,36 @@
 (ns bracketology.core
   (:require [reagent.core :as r :refer [atom]]))
 
-(defonce app-state* (atom {:text "Hello FooBar!"}))
+
+(enable-console-print!)
+
+(defonce app-state* (atom {:text "Hello FooBar!"
+                           :brackets [["foo"
+                                       "bar"
+                                       "biz"
+                                       "boz"]
+                                      ["aaa"
+                                       "bbb"
+                                       "ccc"
+                                       "ddd"]]}))
 
 
-(comment
 
-  (reset! app-state* {:text "FooBar"})
+(defn bracket
+  [teams]
+  (println teams)
+  [:ul
+   (for [team teams]
+     [:li.team team])])
 
-
-  )
 
 (defn app-view
   [app]
   [:div.container
-   [:p (:text @app)]])
+   [:h1 "Bracket"]
+   (for [b (:brackets @app)]
+     [bracket b])
+   ])
 
 
 (defn main []
